5281c9075274703782f0b8d9d4c3954e2231721e
康永 <kangyong@youmatech.com>
2024-05-16 21:48:20 +0800

    feat: 初始化minio-demo

---
+++ heima-leadnews-test/minio-demo/src/main/java/com/heima/minio/MinIOApplication.java
@@ -0,0 +1,18 @@
+package com.heima.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ *
+ * @author Kang Yong
+ * @date 2024/5/16
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MinIOApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinIOApplication.class, args);
+    }
+}
